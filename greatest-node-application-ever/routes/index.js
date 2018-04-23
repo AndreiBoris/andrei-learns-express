695759503087542ece12c0633b21ad63ff25b058
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-04-23 18:39:43 -0400

    Set up baby controller

--- greatest-node-application-ever/routes/index.js
+++ greatest-node-application-ever/routes/index.js
@@ -1,38 +1,42 @@
 const express = require( 'express' )
+const storeController = require( '../controllers/storeController' )
 
 const router = express.Router()
 
-// Do work here
-router.get( '/', ( req, res ) => {
-  /**
-   * Example of sending JSON through Express
-   */
-  // const andy = {
-  //   name: 'Andy',
-  //   age: 40,
-  //   awesome: true,
-  // }
-  // res.json( andy )
-  /**
-   * Example of accessing the URL query parameters
-   */
-  // res.send( req.query )
-  /**
-   * Example of using template with some 'locals' passed to it.
-   */
-  res.render( 'hello', {
-    title: 'I love food',
-    name: 'Andy',
-    dog: req.query.dog,
-  } )
-} )
+router.get( '/', storeController.homePage )
 
-/**
- * Example of using a url slug as a parameter
- */
-router.get( '/reverse/:name', ( req, res ) => {
-  const reveresed = [ ...req.params.name ].reverse().join( '' )
-  res.send( reveresed )
-} )
+// Old Example stuff
+// Do work here
+// router.get( '/', ( req, res ) => {
+//   /**
+//    * Example of sending JSON through Express
+//    */
+//   // const andy = {
+//   //   name: 'Andy',
+//   //   age: 40,
+//   //   awesome: true,
+//   // }
+//   // res.json( andy )
+//   /**
+//    * Example of accessing the URL query parameters
+//    */
+//   // res.send( req.query )
+//   /**
+//    * Example of using template with some 'locals' passed to it.
+//    */
+//   res.render( 'hello', {
+//     title: 'I love food',
+//     name: 'Andy',
+//     dog: req.query.dog,
+//   } )
+// } )
+
+// /**
+//  * Example of using a url slug as a parameter
+//  */
+// router.get( '/reverse/:name', ( req, res ) => {
+//   const reveresed = [ ...req.params.name ].reverse().join( '' )
+//   res.send( reveresed )
+// } )
 
 module.exports = router
