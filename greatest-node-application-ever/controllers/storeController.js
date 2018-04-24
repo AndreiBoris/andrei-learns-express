3cdf09b3b30e5f2af9ac35cb4c7a4d4070852dac
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-04-23 20:27:22 -0400

    Basic model set up

--- greatest-node-application-ever/controllers/storeController.js
+++ greatest-node-application-ever/controllers/storeController.js
@@ -1,8 +1,5 @@
 exports.myMiddleware = ( req, res, next ) => {
   req.name = 'Andy'
-  if ( req.name === 'Andy' ) {
-    throw Error( 'This name is already in use.' )
-  }
   // Set a cookie as part of the response
   res.cookie( 'test-cookie', 'This cookie is a test, yes', { maxAge: 900000 } )
   next()
